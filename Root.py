00e25ae14f3b1c3a6e7c2cef08825b4bca1c0573
Bukhari <fbukhari@visteon.com>
2023-09-15 09:52:14 +0530

    added word wrap for user manual

--- Root.py
+++ Root.py
@@ -153,7 +153,7 @@ class RootClass:
         self.user_manual_frame=tk.Frame(self.user_manual_window)
         self.user_manual_frame.pack(expand=True, fill=tk.BOTH,pady=(20,0))
 
-        self.manual_widget=tk.Text(self.user_manual_frame)
+        self.manual_widget=tk.Text(self.user_manual_frame,wrap="word")
         self.manual_widget.pack(side=tk.LEFT,fill=tk.BOTH,expand=True)
 
         self.user_manual_scrollbar=tk.Scrollbar(self.user_manual_frame)
